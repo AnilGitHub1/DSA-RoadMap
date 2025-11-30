227dfde0d1b0406c71c49f795986fe7597b4dc76
AnilGitHub1 <goramanilnayak@gmail.com>
2025-12-01 05:26:00 +0530

    Anil + 12-1-2025 + BinarySearch

--- Algorithms/Searching/BInarySearch.cs
+++ Algorithms/Searching/BInarySearch.cs
@@ -2,19 +2,24 @@ namespace DSA.Algorithms.Searching
 {
   public static class BinarySearch
   {
+    /// <summary>
+    /// Time O(log(n)) Space O(1)
+    /// returns value from 0 to n, where n is no of elements in the list
+    /// </summary>
+    /// <param name="collection"></param>
+    /// <param name="target"></param>
+    /// <returns></returns>
     public static int Search( IEnumerable<int> collection, int target)
     {
-      var list = collection.ToList();
+      var list = collection.ToList(); // Time O(n) Space O(n)
       int left = 0;
       int right = list.Count - 1;
       while (left <= right)
       {
         int mid  = (right-left)/2 + left;
-        if(list[mid] == target)
-        {
-          return mid;
-        }
-        else if(list[mid] < target)
+        if(list[mid] == target) return mid;
+        
+        if(list[mid] < target)
         {
           left = mid +1;
         }
@@ -24,7 +29,7 @@ namespace DSA.Algorithms.Searching
         }
       }
     
-    return left;
+    return left; //insert index
     }
   }
 }
